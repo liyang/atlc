1236458878b7d49a043f68e3fc39677e3a7854f6
Dr. David Kirkby <drkirkby@kirkbymicrowave.co.uk>
2002-04-09 01:04:49 +0100

    It was printing sym_strip rather than calc_coupler

--- src/usage_calc_coupler.c
+++ src/usage_calc_coupler.c
@@ -6,14 +6,13 @@
 
 void usage_calc_coupler(void)
 {
-  fprintf(stderr,"Usage: sym_strip H w s Er\n\n");
+  fprintf(stderr,"Usage: calc_coupler H w s Er\n\n");
   fprintf(stderr,"calc_coupler calculates Zodd and Zeven for the following coupled lines\n\n");
-  fprintf(stderr,"------^---------------------------------------------------\n");
-  fprintf(stderr,"      |                                        \n");
-  fprintf(stderr,"      |       <---w---><-----s----><---w-->\n");
-  fprintf(stderr,"      H       ---------            --------\n");
-  fprintf(stderr,"      |                               \n");
-  fprintf(stderr,"      |                               \n");
-  fprintf(stderr,"------v----------------------------------------------------\n");
-  //fprintf(stderr,"<----------------------------W---------------------------->\n\n");
+  fprintf(stderr,"------------^------------------------------------------------------------------\n");
+  fprintf(stderr,"            |                                               \n");
+  fprintf(stderr,"            |              <---w---><-----s----><---w-->\n");
+  fprintf(stderr,"            H              ---------            --------\n");
+  fprintf(stderr,"            |                                      \n");
+  fprintf(stderr,"            |                               \n");
+  fprintf(stderr,"------------v------------------------------------------------------------------\n");
 }
