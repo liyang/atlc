f6c6ad5650d08afd84a964a23c8231d0944a1fa9
Dr. David Kirkby <drkirkby@kirkbymicrowave.co.uk>
2002-04-12 01:12:01 +0100

    Changed the file to use blocks of size_t, not ints or longs

--- src/nrutil.c
+++ src/nrutil.c
@@ -3,7 +3,7 @@
 #endif
 #include "definitions.h"
 
-void nrerror(char error_text[])
+void nrerror(char *error_text)
 /* Numerical Recipes standard error handler */
 {
 	fprintf(stderr,"Numerical Recipes run-time error...\n");
@@ -17,7 +17,7 @@ float *vector(long nl, long nh)
 	float *v;
 
 	v=(float *)malloc((size_t) ((nh-nl+1+NR_END)*sizeof(float)));
-	if (!v) nrerror("allocation failure in vector()");
+	if (!v) nrerror((char *) "allocation failure in vector()");
 	return v-nl+NR_END;
 }
 
@@ -27,7 +27,7 @@ int *ivector(long nl, long nh)
 	int *v;
 
 	v=(int *)malloc((size_t) ((nh-nl+1+NR_END)*sizeof(int)));
-	if (!v) nrerror("allocation failure in ivector()");
+	if (!v) nrerror((char *) "allocation failure in ivector()");
 	return v-nl+NR_END;
 }
 
@@ -37,7 +37,7 @@ unsigned char *cvector(long nl, long nh)
 	unsigned char *v;
 
 	v=(unsigned char *)malloc((size_t) ((nh-nl+1+NR_END)*sizeof(unsigned char)));
-	if (!v) nrerror("allocation failure in cvector()");
+	if (!v) nrerror((char *) "allocation failure in cvector()");
 	return v-nl+NR_END;
 }
 
@@ -46,7 +46,7 @@ double *dvector(long nl, long nh)
 {
 	double *v;
 	v=(double *)malloc((size_t) ((nh-nl+1+NR_END)*sizeof(double)));
-	if (!v) nrerror("allocation failure in dvector()");
+	if (!v) nrerror((char *) "allocation failure in dvector()");
 	return v-nl+NR_END;
 }
 
@@ -58,13 +58,13 @@ char **charmatrix(long nrl, long nrh, long ncl, long nch)
 
 	/* allocate pointers to rows */
 	m=(char **) malloc((size_t)((nrow+NR_END)*sizeof(char *)));
-	if (!m) nrerror("allocation failure 1 in matrix()");
+	if (!m) nrerror((char *) "allocation failure 1 in matrix()");
 	m += NR_END;
 	m -= nrl;
 
 	/* allocate rows and set pointers to them */
 	m[nrl]=(char *) malloc((size_t)((nrow*ncol+NR_END)*sizeof(char)));
-	if (!m[nrl]) nrerror("allocation failure 2 in matrix()");
+	if (!m[nrl]) nrerror((char *) "allocation failure 2 in matrix()");
 	m[nrl] += NR_END;
 	m[nrl] -= ncl;
 
@@ -82,13 +82,13 @@ float **matrix(long nrl, long nrh, long ncl, long nch)
 
 	/* allocate pointers to rows */
 	m=(float **) malloc((size_t)((nrow+NR_END)*sizeof(float*)));
-	if (!m) nrerror("allocation failure 1 in matrix()");
+	if (!m) nrerror((char *) "allocation failure 1 in matrix()");
 	m += NR_END;
 	m -= nrl;
 
 	/* allocate rows and set pointers to them */
 	m[nrl]=(float *) malloc((size_t)((nrow*ncol+NR_END)*sizeof(float)));
-	if (!m[nrl]) nrerror("allocation failure 2 in matrix()");
+	if (!m[nrl]) nrerror((char *) "allocation failure 2 in matrix()");
 	m[nrl] += NR_END;
 	m[nrl] -= ncl;
 
@@ -106,13 +106,13 @@ double **dmatrix(long nrl, long nrh, long ncl, long nch)
 
 	/* allocate pointers to rows */
 	m=(double **) malloc((size_t)((nrow+NR_END)*sizeof(double*)));
-	if (!m) nrerror("allocation failure 1 in matrix()");
+	if (!m) nrerror((char *) "allocation failure 1 in matrix()");
 	m += NR_END;
 	m -= nrl;
 
 	/* allocate rows and set pointers to them */
 	m[nrl]=(double *) malloc((size_t)((nrow*ncol+NR_END)*sizeof(double)));
-	if (!m[nrl]) nrerror("allocation failure 2 in matrix()");
+	if (!m[nrl]) nrerror((char *) "allocation failure 2 in matrix()");
 	m[nrl] += NR_END;
 	m[nrl] -= ncl;
 
@@ -130,14 +130,14 @@ int **imatrix(long nrl, long nrh, long ncl, long nch)
 
 	/* allocate pointers to rows */
 	m=(int **) malloc((size_t)((nrow+NR_END)*sizeof(int*)));
-	if (!m) nrerror("allocation failure 1 in matrix()");
+	if (!m) nrerror((char *) "allocation failure 1 in matrix()");
 	m += NR_END;
 	m -= nrl;
 
 
 	/* allocate rows and set pointers to them */
 	m[nrl]=(int *) malloc((size_t)((nrow*ncol+NR_END)*sizeof(int)));
-	if (!m[nrl]) nrerror("allocation failure 2 in matrix()");
+	if (!m[nrl]) nrerror((char *) "allocation failure 2 in matrix()");
 	m[nrl] += NR_END;
 	m[nrl] -= ncl;
 
