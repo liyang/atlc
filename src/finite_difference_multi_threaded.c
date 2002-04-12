8ea939052daf1daa183cc509e39f865fce9368f6
Dr. David Kirkby <drkirkby@kirkbymicrowave.co.uk>
2002-04-12 20:40:10 +0100

    Changed licenece info at top of each source file

--- src/finite_difference_multi_threaded.c
+++ src/finite_difference_multi_threaded.c
@@ -1,3 +1,27 @@
+/* atlc - arbitrary transmission line calculator, for the analysis of
+transmission lines are directional couplers. 
+
+Copyright (C) 2002. Dr. David Kirkby, PhD (G8WRB).
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+USA.
+
+Dr. David Kirkby, e-mail drkirkby@ntlworld.com 
+
+*/
+
 /* This is my first attempt at multi-processing, beyond simple examples
 in books. Hence I suspect it is not efficient */
 
