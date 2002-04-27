080cab64629fa31e43eb0ea6f640c680fe891705
Dr. David Kirkby <drkirkby@kirkbymicrowave.co.uk>
2002-04-27 10:12:55 +0100

    Put more FD code here, to improve readability.

--- src/gui/FiniteDifferenceFrame.cpp
+++ src/gui/FiniteDifferenceFrame.cpp
@@ -30,9 +30,16 @@
 // the event tables connect the wxWindows events with the functions (event
 // handlers) which process them. It can be also done at run-time, but for the
 // simple menu events like this the static method is much simpler.
+
 BEGIN_EVENT_TABLE(FiniteDifferenceFrame, wxFrame)
-    EVT_MENU(ID_Help, FiniteDifferenceFrame::OnHelp)
     EVT_MENU(ID_FileMenuClose, FiniteDifferenceFrame::OnFileMenuClose)
+    EVT_MENU(ID_Help, FiniteDifferenceFrame::OnHelp)
+    EVT_MENU(ID_RectInRect, FiniteDifferenceFrame::OnRectInRect)
+    EVT_MENU(ID_RectInCirc, FiniteDifferenceFrame::OnRectInCirc)
+    EVT_MENU(ID_CircInCirc, FiniteDifferenceFrame::OnCircInCirc)
+    EVT_MENU(ID_CircInRect, FiniteDifferenceFrame::OnCircInRect)
+    EVT_MENU(ID_Cut, FiniteDifferenceFrame::OnCut)
+    EVT_MENU(ID_Paste, FiniteDifferenceFrame::OnPaste)
     EVT_CLOSE(FiniteDifferenceFrame::OnClose) //QUESTION - What is This ???
 END_EVENT_TABLE()
 
@@ -78,29 +85,47 @@ FiniteDifferenceFrame::FiniteDifferenceFrame(const wxString& title, const wxPoin
   menuBar->Append(GenerateBitmapMenu, "&Generate Bitmap");
   menuBar->Append(RunMenu, "&Run");
   menuBar->Append(HelpMenu, "&Help");
-
   SetMenuBar(menuBar);
-
   CreateStatusBar(1);
   SetStatusText("You must now get a bitmap");
-
 }
 
 void FiniteDifferenceFrame::OnHelp(wxCommandEvent& WXUNUSED(event))
 {
-  //help.Display("Main page");
 }
 
 // The following gets executed if the user closes by the box in the very top left.
 void FiniteDifferenceFrame::OnClose(wxCloseEvent& event)
 {
   event.Skip();   
-//  delete
-//  wxConfig::Set(NULL);
 }
 
-// The following gets executed if the user does File->Close on the Round Coax screen.
+void FiniteDifferenceFrame::OnCut(wxCommandEvent& event)
+{
+}
+
+void FiniteDifferenceFrame::OnPaste(wxCommandEvent& event)
+{
+}
+
+// The following gets executed if the user does File->Close
 void FiniteDifferenceFrame::OnFileMenuClose(wxCommandEvent& event)
 {
   wxWindow::Close(TRUE); //Force a closure, window can not resist 
+}
+
+void FiniteDifferenceFrame::OnRectInRect(wxCommandEvent& event)
+{
+}
+
+void FiniteDifferenceFrame::OnCircInCirc(wxCommandEvent& event)
+{
+}
+
+void FiniteDifferenceFrame::OnCircInRect(wxCommandEvent& event)
+{
+}
+
+void FiniteDifferenceFrame::OnRectInCirc(wxCommandEvent& event)
+{
 }
