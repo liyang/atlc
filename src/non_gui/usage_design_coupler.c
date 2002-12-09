117a8f9d22ab092f2d4d3ae7d5228a176f41b344
Dr. David Kirkby <drkirkby@kirkbymicrowave.co.uk>
2002-12-09 18:25:38 +0000

    Changed a few options -l -> -L, remove -h. Made it clearer what is the input port and what is coupled and what is out and what is reflected

--- src/non_gui/usage_design_coupler.c
+++ src/non_gui/usage_design_coupler.c
@@ -26,24 +26,27 @@ Dr. David Kirkby, e-mail drkirkby@ntlworld.com
 
 void usage_design_coupler()
 {
-  fprintf(stderr,"\nUsage: design_coupler -h] [-l ] [-s] [-z ] CF fmin fmax filename\n");
+  fprintf(stderr,"\nUsage: design_coupler [-c] [-P] [-H height] [-L length] [-s fstep] [-z Zo] CF fmin fmax\n");
   fprintf(stderr,"where: \n");
-  fprintf(stderr,"      CF          is the coupling factor required in dB [10 log(P4/P1)]\n");
+  fprintf(stderr,"      CF          is the coupling factor required in dB [10 log(P1/P2)]\n");
   fprintf(stderr,"      fmin        is the minimum frequency (MHz)\n");
   fprintf(stderr,"      fman        is the maximum frequency (MHz)\n");
-  fprintf(stderr,"      filename    is the file to write data to.\n\n");
   fprintf(stderr,"Options are:\n");
-  fprintf(stderr,"     -C           Print copyright, licencing and distribution information.\n");
-  fprintf(stderr,"     -h           help\n");
-  fprintf(stderr,"     -l length    to set the coupler length (m). Defaults to lambda/4\n");
-  fprintf(stderr,"     -s fstep     to set the analysis frequecny in MHz step\n");
+  fprintf(stderr,"     -c           calculate physical dimensions (takes a while)\n");
+  fprintf(stderr,"     -P           Print copyright, licencing and distribution information.\n");
+  fprintf(stderr,"     -H height    to set the height of the enclosure (defaults to 1 unit) \n");
+  fprintf(stderr,"     -L length    to set the coupler length (m). Defaults to lambda/4\n");
+  fprintf(stderr,"     -s fstep     to set the analysis frequency in MHz step\n");
   fprintf(stderr,"     -z Zo        to set the impedance (default 50 Ohms)\n");
-  fprintf(stderr,"Port 3 ****                                           *****Port 4\n");
-  fprintf(stderr,"           *                                         *\n");
-  fprintf(stderr,"           *******************************************\n");
-  fprintf(stderr,"           *******************************************\n");
-  fprintf(stderr,"           *                                         *\n");
-  fprintf(stderr,"Port 1 ***                                            *****Port 2\n");
-  fprintf(stderr,"           <-----------------L----------------------->\n");
-  fprintf(stderr,"\nDrive Port 1, terminate P3 in Zo. P2 is the thru port, P4 is the coupled port\n");
+  fprintf(stderr,"coupled port 2 ****                                        **** reflected port 3");
+  fprintf(stderr,"                  *                                        *\n");
+  fprintf(stderr,"                  ******************************************\n");
+  fprintf(stderr,"                  ******************************************\n");
+  fprintf(stderr,"                  *                                        *\n");
+  fprintf(stderr,"input port 1   ****                                        **** through port 4\n");
+  fprintf(stderr,"                  <-----------------L---------------------->\n");
+  fprintf(stderr,"\nDrive Port 1. The main power comes out from port 4.\n");
+  fprintf(stderr,"A fraction of the forward power comes from port 2\n"); 
+  fprintf(stderr,"A fraction of the reflected power comes from port 3\n"); 
+  fprintf(stderr,"All ports must present an impedance Zo.\n");
 }
