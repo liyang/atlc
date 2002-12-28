1a9f077f09b52cbd801d417e487f26d421c5bdad
Dr. David Kirkby <drkirkby@kirkbymicrowave.co.uk>
2002-12-28 03:05:54 +0000

    added struct to reduce overal use of global variables

--- src/non_gui/fill_rect_in_rect.c
+++ src/non_gui/fill_rect_in_rect.c
@@ -38,9 +38,10 @@ Dr. David Kirkby, e-mail drkirkby@ntlworld.com
 
 extern int W, H, a, b, c, d, w, h;
 
-void fill_image_vector_with_data(unsigned char *image_vector, int colour_Er1, int colour_Er2) 
+void fill_image_vector_with_data(unsigned char *image_vector, int colour_Er1, int colour_Er2, struct transmission_line_properties foo) 
 {
    int i, j, red, green, blue, vector_original=0;
+   fprintf(stderr,"W=%d H=%d\n",W,H);
    for(j=0;j<H;++j)
    {
       for(i=0;i<W; ++i)
