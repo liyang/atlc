baa5987830c78f3e15bd01a8e7b4581e313ca14c
Dr. David Kirkby <drkirkby@kirkbymicrowave.co.uk>
2002-12-31 17:04:22 +0000

    Changed so it displays the fact there are now two options for not writing files; -s for .bmp and -S for .bin

--- src/non_gui/usage_atlc.c
+++ src/non_gui/usage_atlc.c
@@ -35,19 +35,17 @@ void usage_atlc(void)
 #endif
   fprintf(stderr,"options are:\n");
   fprintf(stderr,"  -C       \n      Print Licence and copying information\n");
-  fprintf(stderr,"  -b       \n      Output energy, voltage fields etc, as binary data in addition to bitmaps.\n");
+  fprintf(stderr,"  -s       \n      Skip writing the Ex, Ey, E, V, U and Er files bitmap (.bmp) files\n");
+  fprintf(stderr,"  -S       \n      Skip writing the Ex, Ey, E, V, U and Er files binary (.bin) files\n");
+  fprintf(stderr,"  -v       \n      Verbose option. Print extra data (-vv or -vvv gives even more)\n\n");
   fprintf(stderr,"  -c cutoff\n      where 'cutoff' sets the cutoff criteria - see html docs or man page.\n");
   fprintf(stderr,"  -d rrggbb=Er\n      where the colour rrggbb (in hex) is a (d)ielectric of permittivity Er.\n");
-  fprintf(stderr,"  -F appendfile\n");
-  fprintf(stderr,"      appends all data to 'appendfile'.\n");
   fprintf(stderr,"  -i factor\n      which lightens (factor>%.1f) or darkens (1.0 <factor<%.1f) output bitmaps.\n",
   IMAGE_FIDDLE_FACTOR,IMAGE_FIDDLE_FACTOR);
-#ifdef ENABLE_MP
-  fprintf(stderr,"  -t THREADs. \n      Where THREADs is the number of threads to use (normally best set to \n      the number of cpus).\n");
-#endif
   fprintf(stderr,"  -r rate_multiplier\n      where 'rate_multiplier' sets the rate multipler (called r in source code)\n");
-  fprintf(stderr,"  -s       \n      Skip writing the Ex, Ey, E, V, U and Er files\n");
-  fprintf(stderr,"  -v       \n      Verbose option. Print extra data (-vv or -vvv gives even more)\n\n");
+#ifdef ENABLE_MP
+  fprintf(stderr,"  -t THREADs. \n      Where THREADs is the number of threads to use (normally best set to \n      the number of cpus). The default is %d.\n",MAX_THREADS);
+#endif
 #ifndef ENABLE_MP
   fprintf(stderr,"****************NOTE******************************\n");
   fprintf(stderr,"The -t option setting the number of threads is disabled since either\n");
