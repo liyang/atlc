78a55de7a25c7e3cd08a8591968fe887e172536c
Dr. David Kirkby <drkirkby@kirkbymicrowave.co.uk>
2003-01-09 23:54:59 +0000

    removed string.h and strings.h, as they are not needed

--- src/non_gui/write_fields_for_directional_couplers.c
+++ src/non_gui/write_fields_for_directional_couplers.c
@@ -24,14 +24,6 @@ Dr. David Kirkby, e-mail drkirkby@ntlworld.com
 
 #include "definitions.h"
 
-#ifdef HAVE_STRING_H
-#include <string.h>
-#endif
-
-#ifdef HAVE_STRINGS_H
-#include <strings.h>
-#endif
-
 #ifdef HAVE_STDLIB_H
 #include <stdlib.h>
 #endif
