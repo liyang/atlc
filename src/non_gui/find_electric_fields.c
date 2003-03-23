c4d1f419c554e5bd060ad16e0bd42eeb441984ce
Dr. David Kirkby <drkirkby@kirkbymicrowave.co.uk>
2003-03-23 01:07:34 +0000

    several different algorithms tried to compute E - so far the old one is most accurate

--- src/non_gui/find_electric_fields.c
+++ src/non_gui/find_electric_fields.c
@@ -23,11 +23,12 @@ Dr. David Kirkby, e-mail drkirkby@ntlworld.com
 */
 
 #include "definitions.h"
+#include "exit_codes.h"
 
 extern double **Vij;
 extern char **cell_type;
 extern double **Er;
-extern int width, height;
+extern int width, height, coupler;
 
 /*
 Note, a bitmap has the following pixel nomecture.
@@ -38,8 +39,9 @@ Note, a bitmap has the following pixel nomecture.
 (0,height-1)           (width-1, height-1)
 */
 
+#define USE_OLD_FORMULAS
 
-#ifndef USE_NEW_FORMULAS
+#ifdef USE_OLD_FORMULAS
 
 double find_Ex(int i, int j) 
 {
@@ -81,7 +83,81 @@ double find_Ey(int i, int j)
 
 #endif
 
-#ifdef USE_NEW_FORMULAS
+#ifdef USE_NEW_FORMULAS_A
+
+double find_Ex(int i, int j) 
+{
+  double Ex;
+  if(i < 0 || i > width-1 || j < 0 || j > height-1)
+
+  if(cell_type[i][j]>=0)
+  {
+    if(i>0 && i <width-1)
+      Ex=0.5*(Vij[i-1][j]-Vij[i+1][j]);
+    else
+      Ex=0; /* for now */
+    if((cell_type[i][j]&METAL_LEFT)==METAL_LEFT)
+      Ex=(Vij[i][j]-Vij[i+1][j]);
+    if((cell_type[i][j]&METAL_RIGHT)==METAL_RIGHT)
+      Ex=(Vij[i-1][j]-Vij[i][j]);
+  }
+  else if (cell_type[i][j] == CONDUCTOR_MINUS_ONE_V && Vij[i][j] != -1.0 && coupler ==FALSE)
+  {
+    fprintf(stderr,"in find_Ex celltype[%d][%d] =%d V[%d][%d]=%f\n", i,j,cell_type[i][j],i,j,Vij[i][j]);
+    exit_with_msg_and_exit_code("should be -1 volts", VOLTAGE_NOT_MINUS_ONE_V_AS_EXPECTED);
+  }
+  else if (cell_type[i][j] == CONDUCTOR_PLUS_ONE_V && Vij[i][j] != 1.0)
+  {
+    fprintf(stderr,"in find_Ex  celltype[%d][%d] =%d V[%d][%d]=%f\n", i,j,cell_type[i][j],i,j,Vij[i][j]);
+    exit_with_msg_and_exit_code("should be 1 volts", VOLTAGE_NOT_PLUS_ONE_V_AS_EXPECTED);
+  }
+  else if (cell_type[i][j] == CONDUCTOR_ZERO_V && Vij[i][j] != 0.0)
+  {
+    fprintf(stderr,"in find_Ex celltype[%d][%d] =%d V[%d][%d]=%f\n", i,j,cell_type[i][j],i,j,Vij[i][j]);
+    exit_with_msg_and_exit_code("should be 1 volts", VOLTAGE_NOT_ZERO_V_AS_EXPECTED);
+  }
+  else
+    Ex=0; /* for now */
+  return(Ex);
+}
+
+double find_Ey(int i, int j)
+{
+  double Ey;
+  if(cell_type[i][j]>=0)
+  {
+    if(j > 0 && j < height-2)
+      Ey=0.5*(Vij[i][j+1]-Vij[i][j-1]);
+    else
+      Ey=0; /* for now */
+    if((cell_type[i][j]&METAL_ABOVE)==METAL_ABOVE)
+      Ey=(Vij[i][j+1]-Vij[i][j]);
+    if((cell_type[i][j]&METAL_BELOW)==METAL_BELOW)
+      Ey=(Vij[i][j]-Vij[i][j-1]);
+  }
+  else if (cell_type[i][j] == CONDUCTOR_MINUS_ONE_V && Vij[i][j] != -1.0 && coupler == FALSE)
+  {
+    fprintf(stderr,"in find_Ey  celltype[%d][%d] =%d V[%d][%d]=%f\n", i,j,cell_type[i][j],i,j,Vij[i][j]);
+    exit_with_msg_and_exit_code("should be -1 volts", VOLTAGE_NOT_MINUS_ONE_V_AS_EXPECTED);
+  }
+  else if (cell_type[i][j] == CONDUCTOR_PLUS_ONE_V && Vij[i][j] != 1.0 )
+  {
+    fprintf(stderr,"in find_Ey celltype[%d][%d] =%d V[%d][%d]=%f\n", i,j,cell_type[i][j],i,j,Vij[i][j]);
+    exit_with_msg_and_exit_code("should be 1 volts", VOLTAGE_NOT_PLUS_ONE_V_AS_EXPECTED);
+  }
+  else if (cell_type[i][j] == CONDUCTOR_ZERO_V && Vij[i][j] != 0.0 )
+  {
+    fprintf(stderr,"in find_Ey celltype[%d][%d] =%d V[%d][%d]=%f\n", i,j,cell_type[i][j],i,j,Vij[i][j]);
+    exit_with_msg_and_exit_code("should be 0 volts", VOLTAGE_NOT_ZERO_V_AS_EXPECTED);
+  }
+  else
+    Ey=0; /* for now */
+  return(Ey);
+}
+
+#endif
+
+#ifdef USE_NEW_FORMULAS_B
 double find_Ex(int i, int j) 
 {
   /* Ex = -dV/dx by definition, so is left - right */
@@ -116,6 +192,7 @@ double find_Ey(int i, int j)
   return(Ey);
 }
 #endif
+
 double find_E(int w, int h)
 {
   double Ex, Ey, E;
