e4460d6ecec4857ead3b797676b5acc5f4dad381
Dr. David Kirkby <drkirkby@kirkbymicrowave.co.uk>
2003-06-12 19:37:41 +0100

    gathers data on a wider range of machines - mainly SPARC, HP-UX and Tru64

--- tests/benchmark.c
+++ tests/benchmark.c
@@ -30,6 +30,18 @@ sure the second takes less time than the first. */
 #endif
 
 
+#ifdef HAVE_SYS_PARAM_h /* Try this on HP-UX */
+#include  <sys/param.h>
+#endif
+
+#ifdef HAVE_SYS_PSTAT_H
+#include <sys/pstat.h>
+#endif
+
+#ifdef HAVE_SYS_UNISTD_H 
+#include <sys/unistd.h>
+#endif
+
 #ifdef HAVE_GETSYSINFO /* Only in Tru64 UNIX */
 
 #ifdef HAVE_SYS_SYSINFO_H
@@ -42,14 +54,28 @@ sure the second takes less time than the first. */
 
 #endif 
 
+#define BYTES_PER_MB  1048576
 int main(int argc, char **argv)
 {
   time_t start1, finished1;
   int t1; 
   long num_cpus=0;
-  char *mhz, *eff, *cpus, *str;
-  char *cpu_type, *fpu_type;
+  char *mhz, *eff, *cpus, *str, *max_cpus;
+  char *cpu_type, *fpu_type, *memory;
+  long max_CPUs=0;
   FILE *fp;
+#ifdef HAVE_LONG_LONG
+  long long ram=0;
+#endif
+
+#ifdef PSTAT_STATIC /* Try this on HP-UX */
+#ifdef HAVE_LONG_LONG 
+#ifdef HAVE_PSTAT
+  struct pst_static pst;
+  union pstun pu;
+#endif
+#endif
+#endif
 
 /* Unless the code is multi-threaded, the terms speedup and efficiency have no 
 meaning. There's no point in running the benchmark twice (once as single threaded,
@@ -74,41 +100,108 @@ and if possible their type and speed */
   int  cpuid;
   struct cpu_state cpu_state_buffer;
   struct cpu_info  cpu_info_buffer;
+  long  ramsize_kb=0, ramsize_mb=0;
 #endif /* HAVE_GETSYSINFO */
 
-  cpu_type = (char *) malloc(10000);
-  cpus = (char *) malloc(10000);
-  fpu_type = (char *) malloc(10000);
-  eff = (char *) malloc(100);
   mhz = (char *) malloc(100);
+  cpu_type = (char *) malloc(100);
+  fpu_type = (char *) malloc(1000);
+  max_cpus = (char *) malloc(100);
+  cpus = (char *) malloc(100);
+  memory = (char *) malloc(100);
+  eff = (char *) malloc(100);
+
   strcpy(mhz,"unknown");
-  strcpy(eff,"unknown");
   strcpy(cpu_type,"unknown");
   strcpy(fpu_type,"unknown");
+  strcpy(max_cpus,"unknown");
+  strcpy(cpus,"unknown");
+  strcpy(memory,"unknown");
+  strcpy(eff,"unknown");
 
 #ifdef HAVE_GETSYSINFO /* Only in Tru64 UNIX */
 
   getsysinfo(GSI_CPU,(caddr_t)&cpuid,sizeof(cpuid),0,0);
-  sprintf(cpu_type,"%d",cpuid);
+  sprintf(cpu_type,"%d (whatever that is!)",cpuid);
 
   //getsysinfo(GSI_CPU_STATE,(caddr_t)&cpu_state_buffer,sizeof(cpu_state_buffer),0,0);
   //num_cpus=() cpu_state_buffer.cs_running;
 
   getsysinfo(GSI_CPU_INFO,(caddr_t)&cpu_info_buffer,sizeof(cpu_info_buffer),0,0);
+  if( cpu_info_buffer.mhz > 1) 
   sprintf(mhz,"%d",(int) cpu_info_buffer.mhz);
+
+#ifdef GSI_PHYSMEM
+#ifdef HAVE_LONG_LONG
+  getsysinfo(GSI_PHYSMEM, (caddr_t)&ramsize_kb, sizeof(ramsize_kb), 0, 0); /* in kb */
+  ramsize_mb=ramsize_kb/1024L; /* Mb */
+  sprintf(memory,"%lld",(long long) ramsize_mb);
+#endif /* HAVE_LONG_LONG */
+#endif /* GSI_PHYSMEM */
+
 #endif /* HAVE_GETSYSINFO */
   
 #ifdef _SC_NPROCESSORS_ONLN /* Should be portable */
   num_cpus= (long) sysconf(_SC_NPROCESSORS_ONLN);
   sprintf(cpus,"%ld",num_cpus);
 #endif
-  
-  
+
+#ifdef _SC_NPROCESSORS_MAX
+  max_CPUs=(long) sysconf(_SC_NPROCESSORS_MAX);
+  if(max_CPUs >=1 )
+    sprintf(max_cpus,"%ld",max_CPUs);
+#endif
+
+
+#ifdef HAVE_CPU_GET_MAX  /* Tru64 */
+  max_CPUs=(long) cpu_get_max();
+  if(max_CPUs >=1 )
+    sprintf(max_cpus,"%ld",max_CPUs);
+#endif
+
+/* Find the ram if possible, but taking care not to use a long, as 
+this could be overflowed on a 32-bit system. Hence use a long long */
+
+#ifdef _SC_PHYS_PAGES
+#ifdef _SC_PAGESIZE
+#ifdef HAVE_LONG_LONG
+  if ((long) sysconf(_SC_PHYS_PAGES) > 0L)
+  {
+    if ((long) sysconf(_SC_PAGESIZE) > 0L)
+    {
+      ram=(long long) sysconf(_SC_PAGESIZE);
+      ram*= (long long) sysconf(_SC_PHYS_PAGES);
+      {
+        ram=ram/(long long) BYTES_PER_MB;
+        sprintf(memory,"%lld",ram);
+      }
+    }
+  }
+#endif
+#endif
+#endif
+
+#ifdef PSTAT_STATIC /* Try this on HP-UX */
+#ifdef HAVE_LONG_LONG 
+#ifdef HAVE_PSTAT
+  pu.pst_static=&pst;
+  if( pstat(PSTAT_STATIC,pu,(size_t) sizeof(pst),(size_t) 0, 0) != -1)
+  {
+    ram=(long long) pst.physical_memory;
+    ram*=(long long) pst.page_size;
+    ram=ram/(long long) BYTES_PER_MB;
+    sprintf(memory,"%lld",ram);
+  }
+#endif
+#endif
+#endif
+
+
 #ifdef HAVE_PROCESSOR_INFO
   num_cpus=0;
   for(id=0; id<1024; ++id)
   {
-    if( (processor_info(id, &infop)) == 0)
+    if( processor_info(id, &infop) == 0)
     {
       if(infop.pi_state == P_ONLINE)
       {
@@ -116,7 +209,7 @@ and if possible their type and speed */
         strcpy(cpu_type,infop.pi_processor_type);
         strcpy(fpu_type,infop.pi_fputypes);
         speed=infop.pi_clock;
-        if(speed != 0)
+        if(speed > 1)
           sprintf(mhz,"%d",speed);
       }
     }
@@ -126,7 +219,6 @@ and if possible their type and speed */
 
 /* Whatever happens (single-threaded or multi-theraded, we will check that all the 
 files (the executable atlc and bitmap are both present). */
-
   if(argc !=3 )
   {
     fprintf(stderr,"Usage: path_to_atlc path_to_bitmap\n");
@@ -147,7 +239,6 @@ files (the executable atlc and bitmap are both present). */
     fprintf(stderr,"Can't open the bitmap %s for reading\n",argv[2]);
     exit(3);
   }
-
 /* Assuming we have not exited due to a lack of suitable files present, 
 run the benchmark once - but possibly twice */
 
@@ -186,11 +277,11 @@ so will leave it at the default value of "unknown". */
 
   
 #ifdef ENABLE_POSIX_THREADS
-  printf("0 %d %d %.2f %s %s %s %s %s \n",t1, t2, speedup, cpus, mhz, eff, cpu_type, fpu_type);
+  printf("0 %d %d %.2f %s %s %s %s %s %s %s\n",t1, t2, speedup, cpus, mhz, eff, cpu_type,fpu_type,max_cpus,memory );
   return(0);
 #else 
   strcpy(eff,"1");
-  printf("0 %d %d %.2f %s %s %s %s %s \n",t1, t1, 1.0 , cpus, mhz, eff, cpu_type, fpu_type);
+  printf("0 %d %d %.2f %s %s %s %s %s %s %s \n",t1, t1, 1.0 , cpus, mhz, eff, cpu_type, fpu_type, max_cpus, memory);
   return(0);
 #endif
 
