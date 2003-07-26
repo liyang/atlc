5364daeafe6f651b66b23cf60ba9c132f8e5a43b
Dr. David Kirkby <drkirkby@kirkbymicrowave.co.uk>
2003-07-26 02:58:24 +0100

    increased data storage for cpu_type area, as 40 bytes could be too small. All functions now fully prototyped

--- tests/defs.h
+++ tests/defs.h
@@ -1,6 +1,6 @@
 
 
-#define MAX_SIZE        40
+#define MAX_SIZE        40 
 #define SLEEPTIME       80
 
 #define PROBABLY_LINUX   1
@@ -17,8 +17,15 @@ char mhz[MAX_SIZE], cpus[MAX_SIZE], max_cpus[MAX_SIZE];
 char sysname[MAX_SIZE];
 char nodename[MAX_SIZE], release[MAX_SIZE];
 char version[MAX_SIZE], machine[MAX_SIZE];
-char cpu_type[MAX_SIZE], fpu_type[MAX_SIZE];
+char cpu_type[MAX_SIZE*3], fpu_type[MAX_SIZE];
 char memory[MAX_SIZE], hw_provider[MAX_SIZE];
 char hw_platform[MAX_SIZE], eff[MAX_SIZE], speedup[MAX_SIZE];
+char t1[MAX_SIZE], t2[MAX_SIZE];
+char L1data[MAX_SIZE], L1instruction[MAX_SIZE], L2[MAX_SIZE];
 }a;
+
+int try_portable(struct computer_data *data);
 int try_solaris(struct computer_data *data);
+int try_hpux(struct computer_data *data);
+int try_tru64(struct computer_data *data);
+int try_aix(struct computer_data *data);
