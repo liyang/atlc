4030a7b085404298ddc1539a40094033430eeac3
Dr. David Kirkby <drkirkby@kirkbymicrowave.co.uk>
2003-07-26 02:06:58 +0100

    improved support for HP-UX

--- tests/try_hpux.c
+++ tests/try_hpux.c
@@ -6,7 +6,6 @@
 #ifdef HAVE_STDLIB_H      /* but it will define some variables that we */
 #ifdef HAVE_SYS_TYPES_H   /* later check for, to confirm it's HP-UX */
 #ifdef HAVE_STRING_H
-#ifdef HAVE_SYS_UNISTD_H
 #ifdef HAVE_UNISTD_H 
 #ifdef HAVE_SYS_UTSNAME_H
 #ifdef HAVE_LONG_LONG 
@@ -21,7 +20,9 @@
 #include <sys/unistd.h>
 #include <sys/utsname.h>
 
-#endif
+extern long _CPU_REVISION;
+extern long _FPU_MODEL;
+
 #endif
 #endif
 #endif
@@ -34,9 +35,8 @@
 
 #define BYTES_PER_MB  1048576
 
-int try_hpux(char *mhz, char *cpus, char *max_cpu, char *sysname, \
-char *nodename, char *release, char *version, char *machine, \
-char *cpu_type, char *fpu_type, char *memory,char *manufacturer, char *hw_platform)
+#include "defs.h"
+int try_hpux(struct computer_data *data)
 {
 
 #ifdef HAVE_SYS_PARAM_H 
@@ -45,7 +45,6 @@ char *cpu_type, char *fpu_type, char *memory,char *manufacturer, char *hw_platfo
 #ifdef HAVE_STDLIB_H
 #ifdef HAVE_SYS_TYPES_H
 #ifdef HAVE_STRING_H
-#ifdef HAVE_SYS_UNISTD_H
 #ifdef HAVE_UNISTD_H 
 #ifdef HAVE_SYS_UTSNAME_H
 
@@ -54,44 +53,47 @@ char *cpu_type, char *fpu_type, char *memory,char *manufacturer, char *hw_platfo
 #ifdef HAVE_PSTAT_GETDYNAMIC /* Check for some HP-UX specific bits now */
 #ifdef HAVE_PSTAT_GETPROCESSOR
 #ifdef HAVE_PSTAT
-#ifdef PSTAT_STATIC 
 
-  long num_cpus=0;
-  long long clock_speed_in_Hz;
-  int clock_speed_in_MHz;
+  long max_CPUs=0, CPUs_online=0;
+  long long clock_speed_in_Hz, ram, scclktick;
 
   struct pst_dynamic dynamic_hpux;
+  struct pst_static  static_hpux;
   struct pst_processor psp;
 
-  /* Obtain the number of CPUs on the HP-UX system */
+  /* Obtain the maximum number of CPUs supported on the HP-UX system */
 
   pstat_getdynamic(&dynamic_hpux,(size_t) sizeof(dynamic_hpux),1,0);
-  num_cpus=(long) dynamic_hpux.psd_proc_cnt; ;
   max_CPUs=(long) dynamic_hpux.psd_max_proc_cnt;
-  sprintf(max_cpus,"%ld",max_CPUs);
-  sprintf(cpus,"%ld",num_cpus);
+  if(max_CPUs >=1 )
+    sprintf(data->max_cpus,"%ld",max_CPUs);
+
+  /* Obtain the number of CPUs online on the HP-UXs system */
+  CPUs_online=(long) dynamic_hpux.psd_proc_cnt; ;
+  if( CPUs_online >= 1 )
+    sprintf(data->cpus,"%ld",CPUs_online);
+
+  /* Obtain the of CPU and FPU on the HP-UX box */
+  
+  sprintf(data->cpu_type,"%ld",_CPU_REVISION);
+  sprintf(data->fpu_type,"%ld",_FPU_MODEL);
 
   /* Obtain the RAM on the HP-UX system */
 
-  pstat_getdynamic(&dynamic_hpux,(size_t) sizeof(dynamic_hpux),1,0);
+  pstat_getstatic(&static_hpux,(size_t) sizeof(static_hpux),1,0);
+  ram=(long long) static_hpux.physical_memory;
+  ram*=(long long) static_hpux.page_size;
+  ram=ram/(long long) BYTES_PER_MB;
+  sprintf(data->memory,"%lld",ram);
 
-  pu.pst_static=&pst;
-  if( pstat(PSTAT_STATIC,pu,(size_t) sizeof(pst),(size_t) 0, 0) != -1)
-  {
-    ram=(long long) pst.physical_memory;
-    ram*=(long long) pst.page_size;
-    ram=ram/(long long) BYTES_PER_MB;
-    sprintf(memory,"%lld",ram);
-  }
 
+  /* Obtain the processor speed */
   pstat_getprocessor(&psp, sizeof(psp), 1, 0);
   scclktick=sysconf(_SC_CLK_TCK);
   clock_speed_in_Hz = psp.psp_iticksperclktick * scclktick;
   if(clock_speed_in_Hz > 1)
-    sprintf(mhz,"%ld",clock_speed_in_Hz);
+    sprintf(data->mhz,"%lld",clock_speed_in_Hz/1000000);
   return(0);
-#endif
-#endif
 #endif
 #endif
 #endif
