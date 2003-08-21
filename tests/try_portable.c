9b06c1644cef8a48e67fff1e975d0228dd066603
Dr. David Kirkby <drkirkby@kirkbymicrowave.co.uk>
2003-08-21 01:04:48 +0100

    should not provide error checking for call to uname

--- tests/try_portable.c
+++ tests/try_portable.c
@@ -1,4 +1,7 @@
-/* Try to get data. This will work only on a Solaris machine. */
+/* Try to get data. This should work on just about any 
+system going. However, the amount of data collected 
+is not very much, so other C files, that try to 
+get more information are about. */
 
 #ifdef HAVE_STRING_H
 #include <string.h>
@@ -16,20 +19,39 @@
 #include <sys/utsname.h>
 #endif
 
+#ifdef HAVE_ERRNO_H
+#include <errno.h>
+#endif
 
 #include "defs.h"
+
+extern int errno;
 
 int try_portable(struct computer_data *data)
 {
 #ifdef HAVE_UNAME
   struct utsname name;
-  uname(&name);
-  strcpy(data->sysname,name.sysname);
-  strcpy(data->nodename,name.nodename);
-  strcpy(data->release,name.release);
-  strcpy(data->version,name.version);
-  strcpy(data->machine,name.machine);
-  return(0);
+  int ret;
+
+  ret=uname(&name);
+  if (ret == -1)
+  {
+#ifdef HAVE_ERRNO_H
+    fprintf(stderr,"failure in call to uname in try_portable.c errno=%d\n", errno);
+#else
+    fprintf(stderr,"failure in call to uname in try_portable.c\n");
+#endif
+    return(ret);
+  }
+  else
+  {
+    strcpy(data->sysname,name.sysname);
+    strcpy(data->nodename,name.nodename);
+    strcpy(data->release,name.release);
+    strcpy(data->version,name.version);
+    strcpy(data->machine,name.machine);
+    return(0);
+  }
 #endif
   return(1);
 }
