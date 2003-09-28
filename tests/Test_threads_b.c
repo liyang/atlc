03a327459e331e21d43f0ceade9076263c4aece4
Dr. David Kirkby <drkirkby@kirkbymicrowave.co.uk>
2003-09-28 18:24:57 +0100

    added a copyright message and included config.h

--- tests/Test_threads_b.c
+++ tests/Test_threads_b.c
@@ -1,3 +1,27 @@
+/*
+atlc - arbitrary transmission line calculator, for the analysis of
+transmission lines are directional couplers. 
+
+Copyright (C) 2002. Dr. David Kirkby, PhD (G8WRB).
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either package_version 2
+of the License, or (at your option) any later package_version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+USA.
+
+Dr. David Kirkby, e-mail drkirkby@ntlworld.com 
+
+*/
 
 /* This test program starts by puttting 10 into a 100x1000 array
 then incrments it by 5 and decrements it by 2, one thousand 
@@ -8,6 +32,7 @@ operlapping by a mutex variable. */
 on the array and so any thread can overwrite at any time
 so we would expect jibberish output */
 
+#include "config.h"
 
 #ifdef HAVE_STDIO_H
 #include <stdio.h>
